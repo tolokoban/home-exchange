ea45756198bf9fc4db82b8067f1881bc060cc3ca
Fabien PETITJEAN <fabien.petitjean@epfl.ch>
2024-07-10 11:30:52 +0200

    Tourism and kitchen sections are done

--- src/app/section/kitchen/page.tsx
+++ src/app/section/kitchen/page.tsx
@@ -1,20 +1,56 @@
-import { makeGoto } from "@/app/routes";
-import { IconBack, ViewPanel, ViewStrip } from "@tolokoban/ui";
+import { makeGoto } from "@/app/routes"
+import { IconBack, ViewPanel, ViewStrip } from "@tolokoban/ui"
 
 export default function PageSection() {
-  return (
-    <ViewStrip fullsize orientation="column" template="*1">
-      <ViewPanel
-        color="primary-6"
-        padding="M"
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <IconBack onClick={makeGoto("/")} size="XL" />
-        <h1>Kitchen</h1>
-      </ViewPanel>
-      <ViewPanel color="neutral-5" padding="M" overflow="auto"></ViewPanel>
-    </ViewStrip>
-  );
+    return (
+        <ViewStrip fullsize orientation="column" template="*1">
+            <ViewPanel
+                color="primary-6"
+                padding="M"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <IconBack onClick={makeGoto("/")} size="XL" />
+                <h1>Kitchen</h1>
+            </ViewPanel>
+            <ViewPanel
+                color="neutral-5"
+                padding="M"
+                overflow="auto"
+                display="grid"
+                gridTemplateColumns="1fr min(95vw, 640px) 1fr"
+            >
+                <div></div>
+                <div>
+                    <p>
+                        The house has been build in 1979 on a sloping ground and
+                        it's made mostly of wood.
+                        <br />
+                        So the doors can have their temper at times.
+                    </p>
+                    <p>Here are some videos to help you open and close them.</p>
+                    <ul>
+                        <li>
+                            <b>The induction cooktop</b> (
+                            <a href="video/kitchen.mp4">see video</a>)
+                            <br />
+                            It's very sensitive to water. The smallest drop and
+                            it will start beeping. If it happens, just clean it
+                            up and restart it.
+                        </li>
+                        <li>
+                            There is a hoven and a microwave in the right
+                            column.
+                        </li>
+                        <li>
+                            If you have too much sun, there is a button on the
+                            left side of the hoven column to control the
+                            electric curtain.
+                        </li>
+                    </ul>
+                </div>
+            </ViewPanel>
+        </ViewStrip>
+    )
 }
